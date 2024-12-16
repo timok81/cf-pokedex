c8945cab36bf021f3cdf7fbfd8a54ba8df5f2189
timok81 <timokujansuu@gmail.com>
2024-12-16 14:36:19 +0100

    Added pokemonList

---
+++ README.md
@@ -0,0 +1,2 @@
+Pokedex project for Careerfoundry web development course.
+The goal is to build a small web app that loads data from an external API for viewing.
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-let favoriteFood = 'Pizza';
+let pokemonList = [];
 
-document.write(favoriteFood);
-alert('Hello world');
+pokemonList.push({ name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] });
+pokemonList.push({ name: 'Charmander', height: 0.6, type: ['fire'] });
+pokemonList.push({ name: 'Squirtle', height: 0.5, type: ['water'] });
+pokemonList.push({ name: 'Caterpie', height: 0.3, type: ['bug'] });
+pokemonList.push({ name: 'Weedle', height: 0.3, type: ['bug', 'poison'] });
