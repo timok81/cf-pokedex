8bbb7cbdbb59674c1d0d955b33ae2821208dfac4
timok81 <timokujansuu@gmail.com>
2024-12-16 17:09:21 +0100

    Added for-loop and styling to display pokemons on page

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,13 @@ pokemonList.push({ name: 'Charmander', height: 0.6, type: ['fire'] });
 pokemonList.push({ name: 'Squirtle', height: 0.5, type: ['water'] });
 pokemonList.push({ name: 'Caterpie', height: 0.3, type: ['bug'] });
 pokemonList.push({ name: 'Weedle', height: 0.3, type: ['bug', 'poison'] });
+
+//Displays pokemon information on the page
+for (i = 0; i < pokemonList.length; i++) {
+    if (i === 0) { document.write(`<h1>Pokemons:</h1>`) }
+    document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height})`)
+    if (pokemonList[i].height >= 0.7) {
+        document.write(`<em> - Wow, that's big!</em>`)
+    }
+    document.write(`</p>`);
+}
