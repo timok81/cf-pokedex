941729541a050aecf006be4fe6169ebfcb0ec35c
timok81 <timokujansuu@gmail.com>
2024-12-17 11:45:11 +0100

    Added an IIFE for storing pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,42 @@
-let pokemonList = [];
+//Stores pokemons
+let pokemonRepository = (function () {
+    let pokemonList = [];
 
-pokemonList.push({ name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] });
-pokemonList.push({ name: 'Charmander', height: 0.6, types: ['fire'] });
-pokemonList.push({ name: 'Squirtle', height: 0.5, types: ['water'] });
-pokemonList.push({ name: 'Caterpie', height: 0.3, types: ['bug'] });
-pokemonList.push({ name: 'Weedle', height: 0.3, types: ['bug', 'poison'] });
+    function getAll() {
+        return pokemonList;
+    }
+
+    function add(item) {
+        if (typeof item != "object") { return console.warn("Only objects may be added to list") }
+        if (!Object.keys(item).includes("name")) { return console.warn("Pokemon must have a name") }
+        if (!Object.keys(item).includes("height")) { return console.warn("Pokemon must have a height") }
+        if (!Object.keys(item).includes("types")) { return console.warn("Pokemon must have types") }
+        pokemonList.push(item);
+    }
+
+    function find(name) {
+        return pokemonList.filter((pokemon) => pokemon.name === name);
+    }
+
+    return {
+        getAll,
+        add,
+        find
+    }
+})();
+
+//Adding new pokemons to repository
+pokemonRepository.add({ name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] });
+pokemonRepository.add({ name: 'Charmander', height: 0.6, types: ['fire'] });
+pokemonRepository.add({ name: 'Squirtle', height: 0.5, types: ['water'] });
+pokemonRepository.add({ name: 'Caterpie', height: 0.3, types: ['bug'] });
+pokemonRepository.add({ name: 'Weedle', height: 0.3, types: ['bug', 'poison'] });
 
 //Displays pokemon information on the page
-pokemonList.forEach(function(pokemon) {
-    if (pokemonList.indexOf(pokemon) === 0) { 
+pokemonRepository.getAll().forEach(function (pokemon) {
+    if (pokemonRepository.getAll().indexOf(pokemon) === 0) {
         document.write(`<h1>Pokemons:</h1>`);
-        document.write(`<ul>`);        
+        document.write(`<ul>`);
     }
 
     document.write(`<li>${pokemon.name} (height: ${pokemon.height})`)
@@ -19,5 +45,5 @@ pokemonList.forEach(function(pokemon) {
     }
     document.write(`</li>`);
 
-    if (pokemonList.indexOf(pokemon) === pokemonList.length) { document.write(`</ul>`)}
+    if (pokemonRepository.getAll().indexOf(pokemon) === pokemonRepository.getAll().length) { document.write(`</ul>`) }
 })
