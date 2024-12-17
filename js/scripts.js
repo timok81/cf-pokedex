d49a74e7c74028d14188a584a174c83403f4c3a0
timok81 <timokujansuu@gmail.com>
2024-12-17 11:15:49 +0100

    Changed for-loop to foreach-loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,23 @@
 let pokemonList = [];
 
-pokemonList.push({ name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] });
-pokemonList.push({ name: 'Charmander', height: 0.6, type: ['fire'] });
-pokemonList.push({ name: 'Squirtle', height: 0.5, type: ['water'] });
-pokemonList.push({ name: 'Caterpie', height: 0.3, type: ['bug'] });
-pokemonList.push({ name: 'Weedle', height: 0.3, type: ['bug', 'poison'] });
+pokemonList.push({ name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison'] });
+pokemonList.push({ name: 'Charmander', height: 0.6, types: ['fire'] });
+pokemonList.push({ name: 'Squirtle', height: 0.5, types: ['water'] });
+pokemonList.push({ name: 'Caterpie', height: 0.3, types: ['bug'] });
+pokemonList.push({ name: 'Weedle', height: 0.3, types: ['bug', 'poison'] });
 
 //Displays pokemon information on the page
-for (i = 0; i < pokemonList.length; i++) {
-    if (i === 0) { document.write(`<h1>Pokemons:</h1>`) }
-    document.write(`<p>${pokemonList[i].name} (height: ${pokemonList[i].height})`)
-    if (pokemonList[i].height >= 0.7) {
+pokemonList.forEach(function(pokemon) {
+    if (pokemonList.indexOf(pokemon) === 0) { 
+        document.write(`<h1>Pokemons:</h1>`);
+        document.write(`<ul>`);        
+    }
+
+    document.write(`<li>${pokemon.name} (height: ${pokemon.height})`)
+    if (pokemon.height >= 0.7) {
         document.write(`<em> - Wow, that's big!</em>`)
     }
-    document.write(`</p>`);
-}
+    document.write(`</li>`);
+
+    if (pokemonList.indexOf(pokemon) === pokemonList.length) { document.write(`</ul>`)}
+})
